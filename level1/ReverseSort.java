32265af3f106ea1ab84910d57f2c999a9756a390
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-06-08 20:22:28 +0900

    Create ReverseSort.java

---
+++ level1/ReverseSort.java
@@ -0,0 +1,33 @@
+package programmers.level1;
+
+import java.util.Arrays;
+
+public class ReverseSort {
+
+	public long solution(long n) {
+        
+        String num = String.valueOf(n);
+        String ansnum = "";
+        char [] arr = new char[num.length()];
+        for (int i = 0; i < num.length(); i++) {
+        	arr[i] += num.charAt(i);
+        }
+        
+        Arrays.sort(arr);
+        for (int i = arr.length-1; i >= 0; i--) {
+        	ansnum += arr[i];
+        }
+        
+        long answer = Long.parseLong(ansnum);
+        return answer;
+    }
+	
+	
+	public static void main(String[] args) {
+		ReverseSort rs = new ReverseSort();
+		long test = 118372;
+		System.out.println(rs.solution(test));
+
+	}
+
+}
