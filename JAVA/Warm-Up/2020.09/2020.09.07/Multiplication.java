cfaf88302b4f76564629391f1764087089dfcd89
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-13 17:02:17 +0900

    2020.09.07

---
+++ JAVA/Warm-Up/2020.09/2020.09.07/Multiplication.java
@@ -0,0 +1,13 @@
+class  Multiplication
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<10; i++){
+			System.out.println(i+"´Ü Ãâ·Â");
+			for(int j=1; j<10; j++){
+				System.out.println(i+" X "+j+" = "+(i*j));
+			}
+			System.out.println("-----------");
+		}
+	}
+}
