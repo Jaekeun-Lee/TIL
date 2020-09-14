31dc3d76209cdc5f9a10a3681835cdcb2c2c0bea
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-15 00:28:58 +0900

    Create StringReturn.java

---
+++ JAVA/Programmers/programmers.level1/StringReturn.java
@@ -0,0 +1,19 @@
+package programmers.level1;
+
+public class StringReturn {
+
+	public String solution(int a) {
+		String answer ="";
+		for (int i = 0; i < a; i++) {
+			answer+=(i%2==0)?"¼ö":"¹Ú";
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(new StringReturn().solution(3));
+		System.out.println(new StringReturn().solution(4));
+	}
+
+}
