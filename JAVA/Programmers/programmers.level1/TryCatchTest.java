9523e1febd8916b21456f93f6ee0afb0b0e6b308
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-17 22:55:07 +0900

    Create TryCatchTest.java

---
+++ JAVA/Programmers/programmers.level1/TryCatchTest.java
@@ -0,0 +1,25 @@
+package programmers.level1;
+
+public class TryCatchTest {
+
+	public boolean solution(String s) {
+		if(s.length()==4||s.length()==6) {
+			try {
+				Integer.parseInt(s);
+				return true;
+			}
+			catch(NumberFormatException e) {
+				return false;
+			}
+		}else return false;
+	}
+	public static void main(String[] args) {
+		String s1 = "a234";
+		String s2 = "1234";
+		
+		System.out.println(new TryCatchTest().solution(s1));
+		System.out.println(new TryCatchTest().solution(s2));
+		
+	}
+
+}
