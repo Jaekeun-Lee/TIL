695c995df9c9d0836589f86e691cdd69ca126632
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-19 00:11:54 +0900

    Revert "Test"
    This reverts commit 02f3e35ae19099b7c5eb9dc069afef179bca04dd.

--- JAVA/Programmers/programmers.level1/EvenAndOdd.java
+++ JAVA/Programmers/programmers.level1/EvenAndOdd.java
@@ -2,16 +2,16 @@ package programmers.level1;
 
 public class EvenAndOdd {
 	public String solution(int num) {
-		String answer = "";
-		answer += (num % 2 == 0) ? "Even" : "Odd";
-		return answer;
-	}
-
+        String answer = "";
+        answer+=(num%2==0)?"Even":"Odd";
+        return answer;
+    }
 	public static void main(String[] args) {
 		EvenAndOdd ss = new EvenAndOdd();
-
+		
 		String a = ss.solution(3);
 		System.out.println(a);
+		
 
 	}
 
