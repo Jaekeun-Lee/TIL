3230308440fa189a22255ecbc991f47081b124fc
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-19 00:25:20 +0900

    Update StringReturn.java

--- JAVA/Programmers/programmers.level1/StringReturn.java
+++ JAVA/Programmers/programmers.level1/StringReturn.java
@@ -3,15 +3,15 @@ package programmers.level1;
 public class StringReturn {
 
 	public String solution(int a) {
-		String answer ="";
+		String answer = "";
 		for (int i = 0; i < a; i++) {
-			answer+=(i%2==0)?"수":"박";
+			answer += (i % 2 == 0) ? "수" : "박";
 		}
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(new StringReturn().solution(3));
 		System.out.println(new StringReturn().solution(4));
 	}
