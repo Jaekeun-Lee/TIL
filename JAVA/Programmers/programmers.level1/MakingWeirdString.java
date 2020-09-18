02f3e35ae19099b7c5eb9dc069afef179bca04dd
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-19 00:09:08 +0900

    Test

--- JAVA/Programmers/programmers.level1/MakingWeirdString.java
+++ JAVA/Programmers/programmers.level1/MakingWeirdString.java
@@ -3,18 +3,17 @@ package programmers.level1;
 public class MakingWeirdString {
 
 	public String solution(String s) {
-		
+
 		int count = 0;
 		String answer = "";
-		
-		String [] arr = s.split("");
-		
-		for(String str : arr) {
+
+		String[] arr = s.split("");
+
+		for (String str : arr) {
 			count = str.contains(" ") ? 0 : ++count;
 			answer += count % 2 == 1 ? str.toUpperCase() : str.toLowerCase();
 		}
-		
-		
+
 		return answer;
 	}
 
--- JAVA/Programmers/programmers.level1/StringDescendingSort.java
+++ JAVA/Programmers/programmers.level1/StringDescendingSort.java
@@ -5,28 +5,29 @@ import java.util.Arrays;
 public class StringDescendingSort {
 
 	public String solution(String s) {
-		
-		int [] arr = new int[s.length()];
-		
+
+		int[] arr = new int[s.length()];
+
 		for (int i = 0; i < s.length(); i++) {
-			arr[i]=s.charAt(i);
+			arr[i] = s.charAt(i);
 		}
-		
+
 		Arrays.sort(arr);
-		
+
 		StringBuffer sb = new StringBuffer();
-		
-		for(int i = arr.length-1; i>=0; i--) {
-			sb.append((char)arr[i]);
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			sb.append((char) arr[i]);
 		}
-		
+
 		return sb.toString();
 	}
+
 	public static void main(String[] args) {
 		String s = "Zbcdefg";
-		
+
 		System.out.println(new StringDescendingSort().solution(s));
-		
+
 	}
 
 }
