95a8bccb6b0e1a8bdca638459e6a8c82d6f30f01
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-19 20:37:18 +0900

    Update UnfinishedPlayer.java

--- JAVA/Programmers/src/programmers/level1/UnfinishedPlayer.java
+++ JAVA/Programmers/src/programmers/level1/UnfinishedPlayer.java
@@ -8,17 +8,17 @@ public class UnfinishedPlayer {
 
 		Arrays.sort(participant);
 		Arrays.sort(completion);
-		
+
 		for (int i = 0; i < completion.length; i++) {
-			if(!(participant[i].equals(completion[i]))) {
+			if (!(participant[i].equals(completion[i]))) {
 				return participant[i];
 			}
 		}
-		return participant[participant.length-1];
+		return participant[participant.length - 1];
 	}
 
 	public static void main(String[] args) {
-		String[] participant = { "allen","leo", "eden", "nikola", "vinko", "vinko", "filipa" };
+		String[] participant = { "allen", "leo", "eden", "nikola", "vinko", "vinko", "filipa" };
 		String[] completion = { "nikola", "leo", "filipa", "eden", "vinko", "vinko" };
 
 		System.out.println(UnfinishedPlayer.identify(participant, completion));
