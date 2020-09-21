a6667519173b2345101b88744d66add2a17eebc7
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-21 21:34:41 +0900

    Update MatrixAdd.java

--- JAVA/Programmers/src/programmers/level1/MatrixAdd.java
+++ JAVA/Programmers/src/programmers/level1/MatrixAdd.java
@@ -4,26 +4,24 @@ import java.util.Arrays;
 
 public class MatrixAdd {
 
+	public static int[][] matrixAdd(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr1[0].length];
 
-	public static int[][] matrixAdd(int[][] arr1, int[][] arr2){
-		int [][] answer = new int [arr1.length][arr1[0].length];
-		
 		for (int i = 0; i < answer.length; i++) {
 			for (int j = 0; j < arr1[0].length; j++) {
-				answer[i][j]=arr1[i][j]+arr2[i][j];
+				answer[i][j] = arr1[i][j] + arr2[i][j];
 			}
 		}
 		return answer;
 	}
+
 	public static void main(String[] args) {
-		int[][] arr1 = { { 1, 2 },
-						 { 2, 3 } };
-		
-		int[][] arr2 = { { 3, 4 }, 
-						 { 5, 6 } };
-		
-		int [][] result = matrixAdd(arr1, arr2);
-		for(int[] a:result) {
+		int[][] arr1 = { { 1, 2 }, { 2, 3 } };
+
+		int[][] arr2 = { { 3, 4 }, { 5, 6 } };
+
+		int[][] result = matrixAdd(arr1, arr2);
+		for (int[] a : result) {
 			System.out.println(Arrays.toString(a));
 		}
 	}
