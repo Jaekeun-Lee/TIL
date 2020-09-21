95eb117c0d41f2692d73e3eff378521186cfd2cb
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-21 20:45:56 +0900

    2020.09.21

---
+++ JAVA/Warm-Up/2020.09/2020.09.21/WeirdWord.java
@@ -0,0 +1,31 @@
+package programmer;
+
+public class WeirdWord {
+	public String solution(String s) {
+        String answer = "";
+        s = s.toLowerCase();               // 다 소문자로 변환
+        String tmp[] = s.split(" ",-1);       // 공백 기준으로 나누어 tmp 배열에 저장
+        for(int i = 0;i < tmp.length; i++) {
+            String s2 = tmp[i];
+            if(s2.equals(" ")) continue;
+            for(int index = 0; index < s2.length(); index++) {
+                if(index % 2 == 0) {
+                    answer += (s2.charAt(index)+"").toUpperCase();   
+                    continue;
+                }
+                else{
+                    answer += (s2.charAt(index)+"").toLowerCase();
+                }
+            }
+            answer += " ";
+        }
+        answer = answer.substring(0, answer.length()-1);
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
