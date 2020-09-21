0d41f9ad1cc4676593a8b317b91ffc83d7af3c34
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-21 21:13:37 +0900

    Update FindStringKim.java

--- JAVA/Programmers/src/programmers/level1/FindStringKim.java
+++ JAVA/Programmers/src/programmers/level1/FindStringKim.java
@@ -14,7 +14,7 @@ public class FindStringKim {
 	public static void main(String[] args) {
 		String[] list = { "Jane", "Steve", "Kim" };
 
-		System.out.println("김서방은 "+findKim(list)+"에 있다.");
+		System.out.println("김서방은 " + findKim(list) + "에 있다.");
 	}
 
 }
