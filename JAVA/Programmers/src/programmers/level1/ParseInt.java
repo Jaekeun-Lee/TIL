4151f40570d4966cb90c5bbe96e2a07b0a6241a7
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-22 23:43:35 +0900

    Update ParseInt.java

--- JAVA/Programmers/src/programmers/level1/ParseInt.java
+++ JAVA/Programmers/src/programmers/level1/ParseInt.java
@@ -3,11 +3,10 @@ package programmers.level1;
 public class ParseInt {
 
 	public static void main(String[] args) {
-	
+
 		String s = "1234";
 		int a = Integer.parseInt(s);
-		
-		
+
 		System.out.println(a);
 	}
 
