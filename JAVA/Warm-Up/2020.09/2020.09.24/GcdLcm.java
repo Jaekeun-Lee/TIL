0bf5750156a65a6419577b42439da5a75eafd1d2
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-24 20:09:38 +0900

    Create GcdLcm.java

---
+++ JAVA/Warm-Up/2020.09/2020.09.24/GcdLcm.java
@@ -0,0 +1,35 @@
+package programmers.level1;
+
+public class GcdLcm {
+
+	public static int[] gcdlcm(int n, int m) {
+
+		int gcd = 0;
+		int lcm = 0;
+		int[] answer = new int[2];
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0 && m % i == 0) {
+				gcd = i;
+			}
+		}
+
+		for (int i = n * m; i > 0; i--) {
+			if (i % n == 0 && i % m == 0) {
+				lcm = i;
+			}
+		}
+
+		answer[0] = gcd;
+		answer[1] = lcm;
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		for (int i : gcdlcm(3, 12)) {
+			System.out.println(i);
+		}
+	}
+
+}
