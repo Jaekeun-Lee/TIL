7985b3be940dd49df386697aab69b60d6b22d96c
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-26 22:29:04 +0900

    Update RemoveSmallestNumber.java

--- JAVA/Programmers/src/programmers/level1/RemoveSmallestNumber.java
+++ JAVA/Programmers/src/programmers/level1/RemoveSmallestNumber.java
@@ -6,25 +6,24 @@ import java.util.Collections;
 public class RemoveSmallestNumber {
 
 	public static int[] removeNum(int[] arr) {
-		
-		
-		if (arr.length ==0  || arr[0] == 10) {
-			int [] temp = {-1};
+
+		if (arr.length == 0 || arr[0] == 10) {
+			int[] temp = { -1 };
 			return temp;
 		}
 
-		int [] answer= new int [arr.length-1];
-		ArrayList <Integer> al = new ArrayList<Integer>();
-		for(int i: arr) al.add(i);
-		
+		int[] answer = new int[arr.length - 1];
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for (int i : arr)
+			al.add(i);
+
 		Integer minNum = Collections.min(al);
 		al.remove(minNum);
-		
+
 		for (int i = 0; i < answer.length; i++) {
-			answer[i]=al.get(i);
+			answer[i] = al.get(i);
 		}
-		
-		
+
 		return answer;
 	}
 
@@ -32,9 +31,8 @@ public class RemoveSmallestNumber {
 
 		int[] arr = { 4, 3, 2, 1 };
 
-
-		for(int a : removeNum(arr)) {
-			System.out.print(a+" ");
+		for (int a : removeNum(arr)) {
+			System.out.print(a + " ");
 		}
 	}
 
