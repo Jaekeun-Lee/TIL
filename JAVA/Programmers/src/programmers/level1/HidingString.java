ff0f37be2a3250a90c6a7c8021fef2997d4daa99
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-09-26 22:29:16 +0900

    Update HidingString.java

--- JAVA/Programmers/src/programmers/level1/HidingString.java
+++ JAVA/Programmers/src/programmers/level1/HidingString.java
@@ -2,19 +2,18 @@ package programmers.level1;
 
 public class HidingString {
 
-	
 	public static String hiding(String s) {
 		String temp = "";
-		temp+=s.substring(0, s.length()-4).replaceAll("[0-9]","*");
-		temp+=s.subSequence(s.length()-4, s.length());
-		
+		temp += s.substring(0, s.length() - 4).replaceAll("[0-9]", "*");
+		temp += s.subSequence(s.length() - 4, s.length());
+
 		return temp;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		String phoneNumber2 = "0277778888";
-		
+
 		System.out.println(hiding(phoneNumber2));
 	}
 
