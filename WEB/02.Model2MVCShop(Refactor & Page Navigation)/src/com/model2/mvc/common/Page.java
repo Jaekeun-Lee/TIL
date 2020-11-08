51745827d5fc332d9e847cb948e74e957bff7023
Jaekeun-Lee <64590527+Jaekeun-Lee@users.noreply.github.com>
2020-11-08 18:53:33 +0900

    purchase

--- WEB/02.Model2MVCShop(Refactor & Page Navigation)/src/com/model2/mvc/common/Page.java
+++ WEB/02.Model2MVCShop(Refactor & Page Navigation)/src/com/model2/mvc/common/Page.java
@@ -5,12 +5,12 @@ public class Page {
 	
 	///Field
 	private int currentPage;		// 현재페이지
-	private int totalCount;		// 총 게시물 수
+	private int totalCount;			// 총 게시물 수
 	private int pageUnit;			// 하단 페이지 번호 화면에 보여지는 수
 	private int pageSize;			// 한 페이지당 보여지는 게시물수
 	private int maxPage;			// 최대 페이지 번호(전체 페이지)
-	private int beginUnitPage;	//화면에 보여지는 페이지 번호의 최소수
-	private int endUnitPage;	//화면에 보여지는 페이지 번호의 최대수
+	private int beginUnitPage;		// 화면에 보여지는 페이지 번호의 최소수
+	private int endUnitPage;		// 화면에 보여지는 페이지 번호의 최대수
 	
 	///Constructor
 	public Page() {
@@ -23,8 +23,11 @@ public class Page {
 		this.maxPage = (pageSize == 0) ? totalCount :  (totalCount-1)/pageSize +1;
 		this.currentPage = ( currentPage > maxPage) ? maxPage : currentPage;
 		
-		this.beginUnitPage = ( (currentPage-1) / pageUnit ) * pageUnit +1 ;
+		this.beginUnitPage = ( (currentPage) / pageUnit ) * pageUnit +1 ;
 		
+		if( this.currentPage % pageUnit == 0) {
+			this.beginUnitPage -= this.pageUnit;
+		}
 		if( maxPage <= pageUnit ){
 			this.endUnitPage = maxPage;
 		}else{
